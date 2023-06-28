2c299c959ec145c2a7f76278a0cf74410a91aa8f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-28 22:31:40 +0200

    Create Main.tsx

---
+++ components/Main/Main.tsx
@@ -0,0 +1,15 @@
+import { About } from "../About";
+import { Gallery } from "../Gallery";
+import { MoreAbout } from "../MoreAbout";
+
+const Main = () => {
+  return (
+    <main>
+      <About />
+      <MoreAbout />
+      <Gallery />
+    </main>
+  );
+};
+
+export default Main;
