698b1849f7c34f8fc13527804b3c12bc0cf81d91
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-28 22:30:51 +0200

    Create About.tsx

---
+++ components/About/About.tsx
@@ -0,0 +1,22 @@
+import Button from "../Button";
+import Section from "../Section/Section";
+
+const About = () => {
+  return (
+    <Section className="bg-neutral-800" id="about">
+      <h2 className="text-4xl text-center font-bold mb-8 text-white">O nas</h2>
+      <p className="text-gray-100 drop-shadow-sm mb-8">
+        <span className="text-yellow-300 text-xl">Palladium Fitness Club </span>
+        to wyjątkowe miejsce, które oferuje nie tylko doskonałe wyposażenie, ale
+        również niezrównane doświadczenie treningowe. Nasze studio jest
+        przestrzenią stworzoną z myślą o entuzjastach fitnessu, niezależnie od
+        ich poziomu zaawansowania. Bez względu na to, czy jesteś nowicjuszem czy
+        doświadczonym sportowcem, Palladium Fitness Club zapewnia idealne
+        środowisko do osiągnięcia swoich celów.
+      </p>
+      <Button className="bg-yellow-300">Dołącz</Button>
+    </Section>
+  );
+};
+
+export default About;
