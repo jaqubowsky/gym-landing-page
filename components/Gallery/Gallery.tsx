ed2b3f2b035ee876c4ee78e79199cdc5a4003141
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-01 15:29:54 +0200

    Change styling

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -14,7 +14,8 @@ const SLIDES = Array.from(Array(SLIDES_COUNT).keys());
 const Gallery = () => {
   return (
     <Section id="gallery">
-      <h2 className="text-4xl font-bold mb-8">Galeria</h2>
+      <h2 className="text-4xl font-bold mb-4">Galeria</h2>
+      <h5 className="font-bold mb-8">Zobacz zdjęcia z naszego klubu.</h5>
       <Carousel slides={SLIDES} options={OPTIONS} />
     </Section>
   );
