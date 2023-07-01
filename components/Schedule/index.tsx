81d175a1fa050027be39cb9196bab2ab59286a0c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-01 15:29:32 +0200

    Create index.tsx

---
+++ components/Schedule/index.tsx
@@ -0,0 +1,4 @@
+export { default as Schedule } from "./Schedule";
+export { default as timetable } from "./timetable";
+export { default as ScheduleBox } from "./ScheduleBox";
+export { default as ScheduleEvent } from "./ScheduleEvent";
