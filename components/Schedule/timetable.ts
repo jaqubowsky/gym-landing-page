0adbf29cb1c187095dd4308d8319272a6695c754
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-01 15:29:19 +0200

    Create timetable for Schedule

---
+++ components/Schedule/timetable.ts
@@ -0,0 +1,103 @@
+const schedule = [
+  {
+    weekday: "poniedziałek",
+    events: [
+      {
+        eventName: "Zajęcia taneczne",
+        start: "16:00",
+        to: "17:00",
+      },
+      {
+        eventName: "Zajęcia taneczne",
+        start: "17:00",
+        to: "18:00",
+      },
+      {
+        eventName: "Zajęcia taneczne",
+        start: "18:00",
+        to: "19:30",
+      },
+      {
+        eventName: "Funkcjonalne ciało",
+        start: "20:00",
+        to: "21:00",
+      },
+    ],
+  },
+  {
+    weekday: "wtorek",
+    events: [
+      {
+        eventName: "Stacja formy",
+        start: "17:00",
+        to: "18:00",
+      },
+      {
+        eventName: "Muay thai dzieci",
+        start: "17:45",
+        to: "19:00",
+      },
+      {
+        eventName: "Muay thai",
+        start: "19:00",
+        to: "20:30",
+      },
+    ],
+  },
+  {
+    weekday: "Środa",
+    events: [
+      {
+        eventName: "Boks / kickboxing dzieci",
+        start: "17:00",
+        to: "18:00",
+      },
+      {
+        eventName: "Interval class",
+        start: "19:15",
+        to: "20:15",
+      },
+    ],
+  },
+  {
+    weekday: "Czwartek",
+    events: [
+      {
+        eventName: "Joga",
+        start: "17:30",
+        to: "18:30",
+      },
+      {
+        eventName: "Muay thai dzieci",
+        start: "17:45",
+        to: "19:00",
+      },
+      {
+        eventName: "Muay thai",
+        start: "19:00",
+        to: "20:30",
+      },
+    ],
+  },
+  {
+    weekday: "Piątek",
+    events: [
+      {
+        eventName: "Boks / kickboxing dzieci",
+        start: "17:00",
+        to: "18:00",
+      },
+      {
+        eventName: "Speedball / sztanga",
+        start: "19:15",
+        to: "20:15",
+      },
+    ],
+  },
+  {
+    weekday: "Sobota / Niedziela",
+    events: [],
+  },
+];
+
+export default schedule;
