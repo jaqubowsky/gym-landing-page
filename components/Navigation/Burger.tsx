fbe82a79b9b67ba1779c2c115452fb47204e68cc
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-02 15:07:52 +0200

    Update burger animations

--- components/Navigation/Burger.tsx
+++ components/Navigation/Burger.tsx
@@ -17,28 +17,28 @@ const Burger: FC<Burger> = ({ isOpen, onClick }) => {
       <motion.div
         animate={{
           rotate: isOpen ? 45 : 0,
-          y: !isOpen ? 0 : -5,
+          y: !isOpen ? 0 : 12,
         }}
         className={`relative bg-white h-1 w-8" ${
           isOpen ? "bg-yellow-300" : ""
         }`}
-      ></motion.div>
+      />
       <motion.div
         animate={{
           opacity: isOpen ? 0 : 1,
-          transform: isOpen ? "translateY(-20px)" : "translateY(0px)",
+          transform: isOpen ? "translateX(20px)" : "translateX(0px)",
         }}
         className={`relative bg-white h-1 w-6" ${
           isOpen ? "bg-yellow-300" : ""
         }`}
-      ></motion.div>
+      />
       <motion.div
         animate={{
           rotate: isOpen ? -45 : 0,
-          y: !isOpen ? 0 : -27,
+          y: !isOpen ? 0 : -10,
         }}
         className={`relative bg-white h-1 w-8 ${isOpen ? "bg-yellow-300" : ""}`}
-      ></motion.div>
+      />
     </motion.div>
   );
 };
