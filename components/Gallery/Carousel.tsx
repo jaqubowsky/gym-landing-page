aa63551f281fda73bf429ea3636c23d58ff351e8
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-02 15:06:58 +0200

    Use cloudinary

--- components/Gallery/Carousel.tsx
+++ components/Gallery/Carousel.tsx
@@ -2,32 +2,45 @@
 
 import { FC } from "react";
 import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
-import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
-import imageByIndex from "@/utils/getImageByIndex";
+import CloudinaryImage from "../CloudinaryImage";
+import { image } from "../Lightbox/Lightbox";
+import { useRouter } from "next/navigation";
 
 type PropType = {
-  slides: number[];
+  images: image[];
   options?: EmblaOptionsType;
 };
 
-const Carousel: FC<PropType> = ({ options, slides }) => {
+const Carousel: FC<PropType> = ({ options, images }) => {
+  const router = useRouter();
+
+  const navigateToGallery = () => {
+    router.push("/gallery");
+  };
+
+  const slidesEl = images.map((image) => {
+    return (
+      <div
+        className="relative h-64 flex-[0_0_auto] w-full md:w-1/3 gap-6"
+        key={image.id}
+        onClick={navigateToGallery}
+      >
+        <CloudinaryImage
+          fill
+          className="object-cover h-full w-full"
+          src={image.src}
+          alt="Your alt text"
+        />
+      </div>
+    );
+  });
+
   const [emblaRef] = useEmblaCarousel(options, [Autoplay()]);
 
   return (
     <div className="overflow-hidden w-full" ref={emblaRef}>
-      <div className="flex">
-        {slides.map((index) => (
-          <div className="relative h-64 flex-[0_0_auto] w-full md:w-1/3 gap-6" key={index}>
-            <Image
-              fill
-              className="object-cover"
-              src={imageByIndex(index)}
-              alt="Your alt text"
-            />
-          </div>
-        ))}
-      </div>
+      <div className="flex">{slidesEl}</div>
     </div>
   );
 };
--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -1,22 +1,35 @@
 import { EmblaOptionsType } from "embla-carousel-react";
 import Section from "../Section/Section";
 import Carousel from "./Carousel";
-import { images } from "@/utils/getImageByIndex";
+import ButtonLink from "../ButtonLink";
+import { mapImageResources, search } from "@/lib/cloudinary";
+import { Suspense } from "react";
 
 const OPTIONS: EmblaOptionsType = {
   loop: true,
   slidesToScroll: "auto",
   containScroll: "trimSnaps",
 };
-const SLIDES_COUNT = images.length;
-const SLIDES = Array.from(Array(SLIDES_COUNT).keys());
 
-const Gallery = () => {
+const Gallery = async () => {
+  const { resources } = await search({
+    expression: 'folder="gallery" AND resource_type="image"',
+    max_results: 4,
+    direction: "desc",
+  });
+
+  const images = mapImageResources(resources);
+
   return (
     <Section id="gallery">
       <h2 className="text-4xl font-bold mb-4">Galeria</h2>
       <h5 className="font-bold mb-8">Zobacz zdjęcia z naszego klubu.</h5>
-      <Carousel slides={SLIDES} options={OPTIONS} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Carousel images={images} options={OPTIONS} />
+      </Suspense>
+      <ButtonLink className="bg-yellow-300 mt-6" href="/gallery">
+        Zobacz więcej
+      </ButtonLink>
     </Section>
   );
 };
