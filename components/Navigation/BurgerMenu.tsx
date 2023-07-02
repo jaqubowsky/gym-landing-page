f2b846fb947e5f20ef8ba9ffa3aa7317261d8912
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-02 15:08:05 +0200

    Fix styling, use cloudinary image

--- components/Navigation/BurgerMenu.tsx
+++ components/Navigation/BurgerMenu.tsx
@@ -4,7 +4,9 @@ import { FC, ReactNode, useState } from "react";
 import Burger from "./Burger";
 import { AnimatePresence, motion } from "framer-motion";
 import logo from "@/assets/images/gym_logo.png";
-import Logo from "../Logo";
+import NavigationItem from "./NavigationItem";
+import Image from "next/image";
+import CloudinaryImage from "../CloudinaryImage";
 
 interface BurgerMenu {
   children: ReactNode;
@@ -13,20 +15,32 @@ interface BurgerMenu {
 const BurgerMenu: FC<BurgerMenu> = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+    document.body.classList.toggle("overflow-hidden");
+  };
+
   return (
     <>
-      <Logo logo={logo} />
-      <Burger isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+      <Burger isOpen={isOpen} onClick={toggleOpen} />
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="absolute left-0 top-0 w-full h-screen bg-neutral-700 flex items-center justify-center"
+            className="absolute left-0 top-0 w-screen h-screen bg-neutral-700 flex items-center justify-center"
             initial={{ opacity: 0, x: "-100%" }}
             animate={{ opacity: 1, height: "100vh", x: 0 }}
             exit={{ opacity: 0, x: "100%" }}
             transition={{ duration: 0.2 }}
           >
             <div className="flex flex-col items-center justify-around h-3/4">
+              <NavigationItem href="/" target="_self">
+                <CloudinaryImage
+                  src="/gym_logo_immgmy"
+                  alt="logo"
+                  height={200}
+                  width={200}
+                />
+              </NavigationItem>
               {children}
             </div>
           </motion.div>
