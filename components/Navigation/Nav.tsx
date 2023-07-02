027deea96e4cf7008fb67fe3540091d02482fe39
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-02 15:08:28 +0200

    Fix Nav styling

--- components/Navigation/Nav.tsx
+++ components/Navigation/Nav.tsx
@@ -10,7 +10,7 @@ const Nav: FC<Nav> = ({ className, children }) => {
   return (
     <nav
       className={clsx(
-        "z-50 top-0 items-center left-0 w-full flex sticky justify-between bg-neutral-900 drop-shadow-md-",
+        "z-30 inset-0 items-center flex sticky justify-between md:justify-around bg-neutral-900 drop-shadow-md",
         className
       )}
     >
