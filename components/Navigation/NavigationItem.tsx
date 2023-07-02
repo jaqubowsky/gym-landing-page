14e69b206296446c244dff1f8ca4ccc0a964fd6c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-02 15:08:49 +0200

    Add target prop

--- components/Navigation/NavigationItem.tsx
+++ components/Navigation/NavigationItem.tsx
@@ -5,15 +5,17 @@ import { FC, ReactNode } from "react";
 interface NavigationItem extends LinkProps {
   children: ReactNode;
   className?: string;
+  target?: string;
 }
 
 const NavigationItem: FC<NavigationItem> = ({
   children,
+  target = "_blank",
   className,
   ...props
 }) => {
   return (
-    <Link className={clsx("drop-shadow-md font-bold text-white", className)} {...props}>
+    <Link target={target} className={clsx("drop-shadow-md font-bold text-white", className)} {...props}>
       {children}
     </Link>
   );
