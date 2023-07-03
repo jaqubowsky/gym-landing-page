ed9a9b51a88287840eea5ba7d61d2ac1a5ad9aa2
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-03 21:46:41 +0200

    RWD styles, use Image instead of CldImage

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,12 @@
-import { CldImage } from "next-cloudinary";
 import Section from "../Section/Section";
 import IconsGrid from "./IconsGrid";
-import CloudinaryImage from "../CloudinaryImage";
+import Image from "next/image";
+import logo2 from "@/assets/images/logo2.png";
 
 const Footer = () => {
   return (
     <Section id="footer" className="bg-yellow-300">
-      <div className="grid grid-cols-1 items-center justify-center gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-6 mb-8">
         <div className="flex flex-col gap-2">
           <h2 className="font-bold">Masz pytania?</h2>
           <h5>Nasza recepcja czeka na twój telefon.</h5>
@@ -17,6 +17,15 @@ const Footer = () => {
             <span className="block">06-100 Pułtusk</span>
           </h5>
         </div>
+        <div>
+          <Image
+            className="bg-black rounded-full my-4 h-full w-full"
+            src={logo2}
+            width={200}
+            height={200}
+            alt="logo2"
+          />
+        </div>
         <div className="flex items-center flex-col gap-2">
           <div className="flex gap-6">
             <span className="font-bold">Pon.-Pt.</span>
@@ -26,16 +35,13 @@ const Footer = () => {
             <span className="font-bold">Sob.-Nd.</span>
             <span>8:00-18:00</span>
           </div>
-            <CloudinaryImage
-              className="bg-black rounded-full my-4 h-full w-full"
-              src="/logo2_dwpoia"
-              width={200}
-              height={200}
-              alt="logo2"
-            />
-          <IconsGrid />
+          <p className="mb-4">recepcja.braingym@op.pl</p>
         </div>
       </div>
+      <IconsGrid />
+      <h2 className="font-bold text-sm mt-4">
+        Copyright © Palladium Fitness Club 2023
+      </h2>
     </Section>
   );
 };
