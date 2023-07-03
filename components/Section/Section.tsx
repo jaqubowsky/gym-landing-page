a0f9d53c5ab179974e815ace7660a38c24c9f340
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-03 21:42:23 +0200

    Ability to add styles for inner div, fix types, add style prop

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,19 +1,19 @@
 import clsx from "clsx";
 import { FC, ReactNode } from "react";
 
-interface Section {
+interface Section extends React.HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
-  id: string;
-  className?: string;
+  innerStyle?: string;
 }
 
-const Section: FC<Section> = ({ children, className, id }) => {
+const Section: FC<Section> = ({ children, className, id, style, innerStyle }) => {
   return (
     <section
       id={id}
-      className={clsx("flex-container flex-col w-full text-center", className)}
+      style={style}
+      className={clsx("flex-container flex-col w-full text-center mx-auto", className)}
     >
-      <div className="max-w-6xl px-6 py-12 w-full flex-container flex-col">
+      <div className={clsx("max-w-6xl md:max-w-2xl px-6 py-12 w-full flex-container flex-col", innerStyle)}>
         {children}
       </div>
     </section>
