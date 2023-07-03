6a2e99324847cf2055059f38ad35031afdf279bc
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-03 21:46:02 +0200

    Delete suspense, fix max_results on images, RWD styles

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -3,7 +3,6 @@ import Section from "../Section/Section";
 import Carousel from "./Carousel";
 import ButtonLink from "../ButtonLink";
 import { mapImageResources, search } from "@/lib/cloudinary";
-import { Suspense } from "react";
 
 const OPTIONS: EmblaOptionsType = {
   loop: true,
@@ -14,19 +13,19 @@ const OPTIONS: EmblaOptionsType = {
 const Gallery = async () => {
   const { resources } = await search({
     expression: 'folder="gallery" AND resource_type="image"',
-    max_results: 4,
+    max_results: "7",
     direction: "desc",
   });
 
   const images = mapImageResources(resources);
 
   return (
-    <Section id="gallery">
-      <h2 className="text-4xl font-bold mb-4">Galeria</h2>
-      <h5 className="font-bold mb-8">Zobacz zdjęcia z naszego klubu.</h5>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Carousel images={images} options={OPTIONS} />
-      </Suspense>
+    <Section id="gallery" innerStyle="md:max-w-6xl">
+      <h2 className="text-6xl font-bold mb-4">Galeria</h2>
+      <h5 className="font-bold mb-8 lg:text-2xl">
+        Zobacz zdjęcia z naszego klubu.
+      </h5>
+      <Carousel images={images} options={OPTIONS} />
       <ButtonLink className="bg-yellow-300 mt-6" href="/gallery">
         Zobacz więcej
       </ButtonLink>
