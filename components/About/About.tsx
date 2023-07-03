c7db7f693e4011abb0fbf3b26649b59a19271f4f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-03 21:47:08 +0200

    RWD styles, fix routes and naming

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,14 +2,13 @@
 
 import ButtonLink from "../ButtonLink";
 import Section from "../Section/Section";
-import { CldImage } from "next-cloudinary";
 
 const About = () => {
   return (
     <Section className="bg-neutral-800" id="about">
-      <h2 className="text-4xl text-center font-bold mb-8 text-white">O nas</h2>
-      <p className="text-gray-100 drop-shadow-sm mb-8">
-        <span className="text-yellow-300 text-xl">Palladium Fitness Club </span>
+      <h2 className="text-6xl text-center font-bold mb-8 text-white">O nas</h2>
+      <p className="text-gray-100 drop-shadow-sm mb-8 lg:text-2xl">
+        <span className="text-yellow-300 text-xl lg:text-3xl">Palladium Fitness Club </span>
         to wyjątkowe miejsce, które oferuje nie tylko doskonałe wyposażenie, ale
         również niezrównane doświadczenie treningowe. Nasze studio jest
         przestrzenią stworzoną z myślą o entuzjastach fitnessu, niezależnie od
@@ -17,8 +16,8 @@ const About = () => {
         doświadczonym sportowcem, Palladium Fitness Club zapewnia idealne
         środowisko do osiągnięcia swoich celów.
       </p>
-      <ButtonLink href="/" className="bg-yellow-300">
-        Dołącz
+      <ButtonLink href="/about" className="bg-yellow-300">
+        Dowiedz się więcej!
       </ButtonLink>
     </Section>
   );
