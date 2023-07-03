dabaf2377f32a560a928365a3cfac5f5d67b61bc
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-03 21:47:48 +0200

    Delete Suspense, add RWD styles

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,6 +1,5 @@
 import Section from "@/components/Section/Section";
 import { Lightbox } from "@/components/Lightbox";
-import { Suspense } from "react";
 import { mapImageResources, search } from "@/lib/cloudinary";
 
 export default async function GalleryPage() {
@@ -9,21 +8,15 @@ export default async function GalleryPage() {
     direction: "desc",
   });
 
-  const images = mapImageResources(resources);
+  const imagesArr = mapImageResources(resources);
 
   return (
     <>
       <Section id="" className="bg-yellow-300">
         <h4 className="font-bold text-5xl drop-shadow-md">Galeria</h4>
       </Section>
-      <Section id="gallery">
-        <Suspense fallback={<div>Loading...</div>}>
-          {images.length > 0 ? (
-            <Lightbox imagesArr={images} />
-          ) : (
-            <h3>Brak zdjęć w naszej galerii...</h3>
-          )}
-        </Suspense>
+      <Section id="gallery" innerStyle="md:max-w-6xl">
+        <Lightbox imagesArr={imagesArr} />
       </Section>
     </>
   );
