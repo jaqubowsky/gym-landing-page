a327b0884a0c57fde22ccb9ff62439ada64796b3
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-05 01:08:31 +0200

    Prettier, rwd styles

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -6,14 +6,28 @@ interface Section extends React.HTMLAttributes<HTMLDivElement> {
   innerStyle?: string;
 }
 
-const Section: FC<Section> = ({ children, className, id, style, innerStyle }) => {
+const Section: FC<Section> = ({
+  children,
+  className,
+  id,
+  style,
+  innerStyle,
+}) => {
   return (
     <section
       id={id}
       style={style}
-      className={clsx("flex-container flex-col w-full text-center mx-auto", className)}
+      className={clsx(
+        "flex-container flex-col w-full text-center mx-auto",
+        className
+      )}
     >
-      <div className={clsx("max-w-6xl md:max-w-2xl px-6 py-12 w-full flex-container flex-col", innerStyle)}>
+      <div
+        className={clsx(
+          "max-w-2xl md:max-w-4xl px-6 py-12 w-full flex-container flex-col",
+          innerStyle
+        )}
+      >
         {children}
       </div>
     </section>
