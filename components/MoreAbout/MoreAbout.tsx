3b454f236be345014e6557a09c01e84f2102d609
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-05 01:09:25 +0200

    Add image instead of div with bg-img

--- components/MoreAbout/MoreAbout.tsx
+++ components/MoreAbout/MoreAbout.tsx
@@ -1,6 +1,7 @@
 import ButtonLink from "../ButtonLink";
 import about from "../../assets/images/about.png";
 import Section from "../Section/Section";
+import Image from "next/image";
 
 const MoreAbout = () => {
   return (
@@ -9,24 +10,27 @@ const MoreAbout = () => {
         Nie tylko{" "}
         <span className="text-yellow-300 drop-shadow-sm">siłownia</span>
       </h2>
-      <div className="mb-6">
-        <p className="text-white drop-shadow-sm mb-6 lg:text-2xl">
-          Odkryj różnorodność i emocje w{" "}
-          <span className="text-yellow-300 drop-shadow-sm">
-            Palladium Fitness Club
-          </span>
-          ! U nas znajdziesz nie tylko siłownię, ale także szereg innych
-          aktywności, takich jak boks, zajęcia taneczne, funkcjonalne ciało,
-          muay thai, speedball i interval class. Dołącz do nas już teraz i ciesz
-          się pasją, wyzwaniami i radością, jakie oferuje nasze klubowe
-          doświadczenie fitness!
-        </p>
-        <div
-          className="col-span-3 bg-center bg-cover md:bg-contain md:bg-fixed w-full h-[400px] md:h-[600px] bg-no-repeat"
-          style={{
-            backgroundImage: `url(${about.src})`,
-          }}
-        ></div>
+      <div className="flex flex-col md:flex-row gap-6 items-center">
+        <div className="mb-6 md:w-1/2 md:text-3xl flex md:items-center">
+          <p className="text-white drop-shadow-sm mb-6">
+            Odkryj różnorodność i emocje w{" "}
+            <span className="text-yellow-300 drop-shadow-sm">
+              Palladium Fitness Club
+            </span>
+            ! U nas znajdziesz nie tylko siłownię, ale także szereg innych
+            aktywności, takich jak boks, zajęcia taneczne, funkcjonalne ciało,
+            muay thai, speedball i interval class. Dołącz do nas już teraz i
+            ciesz się pasją, wyzwaniami i radością, jakie oferuje nasze klubowe
+            doświadczenie fitness!
+          </p>
+        </div>
+        <div className="flex-1 max-w-[350px] mb-6">
+          <Image
+            src={about}
+            alt="Paweł Brejnak"
+            className="object-contain h-full w-full"
+          />
+        </div>
       </div>
       <ButtonLink href="/schedule" className="bg-yellow-300 w-full md:w-1/2">
         Plan zajęć
