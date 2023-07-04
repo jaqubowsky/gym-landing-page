100d8ea93c6c9e9cd4813ae7602ead0c3bbe29f8
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-05 01:10:41 +0200

    Change href, prettier

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -8,7 +8,9 @@ const About = () => {
     <Section className="bg-neutral-800" id="about">
       <h2 className="text-6xl text-center font-bold mb-8 text-white">O nas</h2>
       <p className="text-gray-100 drop-shadow-sm mb-8 lg:text-2xl">
-        <span className="text-yellow-300 text-xl lg:text-3xl">Palladium Fitness Club </span>
+        <span className="text-yellow-300 text-xl lg:text-3xl">
+          Palladium Fitness Club{" "}
+        </span>
         to wyjątkowe miejsce, które oferuje nie tylko doskonałe wyposażenie, ale
         również niezrównane doświadczenie treningowe. Nasze studio jest
         przestrzenią stworzoną z myślą o entuzjastach fitnessu, niezależnie od
@@ -16,7 +18,7 @@ const About = () => {
         doświadczonym sportowcem, Palladium Fitness Club zapewnia idealne
         środowisko do osiągnięcia swoich celów.
       </p>
-      <ButtonLink href="/about" className="bg-yellow-300">
+      <ButtonLink href="/meet-us" className="bg-yellow-300">
         Dowiedz się więcej!
       </ButtonLink>
     </Section>
