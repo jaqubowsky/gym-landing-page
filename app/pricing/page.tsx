0133aa447add581c622cc670d40b0a099ec61c7d
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-05 01:12:58 +0200

    Prettier command

---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import Section from "@/components/Section/Section";
+
+export default function Pricing() {
+  return (
+    <Section id="pricing">
+      <h2 className="text-4xl font-bold mb-4">Cennik</h2>
+    </Section>
+  );
+}
