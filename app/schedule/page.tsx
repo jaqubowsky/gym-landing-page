37445a6c2a0e7a5e88f29f26302823dd1be19d86
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-05 01:10:13 +0200

    Update width style

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -3,7 +3,7 @@ import Section from "@/components/Section/Section";
 
 export default function Scheudle() {
   return (
-    <Section id="scheudle" className="max-w-max">
+    <Section id="scheudle">
       <h2 className="text-4xl font-bold mb-4">Grafik</h2>
       <h5 className="font-bold mb-8">
         Jesteśmy dostępni dla ciebie cały tydzień!
