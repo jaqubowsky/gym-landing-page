5a026fe50ce28ef35b3c48e03872a15b12a6c70a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-05 11:45:11 +0200

    Semantic html

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,13 +11,13 @@ const Header = () => {
         backgroundImage: `url(${background.src})`,
       }}
     >
-      <div className="bg-trBlack px-6 md:px-12 lg:px-20 py-12 rounded-xl drop-shadow-md">
+      <div className="bg-trBlack px-6 md:px-12 lg:px-20 py-12 rounded-xl drop-shadow-md flex flex-col">
         <h1 className="mb-6 text-5xl drop-shadow-xl uppercase font-bold text-yellow-300 break-words">
           Palladium fitness<span className=" text-gray-100 block">Club</span>
         </h1>
-        <h5 className="drop-shadow-xl text-white mb-10">
+        <span className="drop-shadow-xl text-white mb-10">
           Najlepsza siłownia w Pułtusku
-        </h5>
+        </span>
         <ButtonLink href="/pricing" intent="yellow" size="large">
           Dołącz
         </ButtonLink>
