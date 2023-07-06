47c90097fa660b81468949ed07b24c0857679d75
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-07 01:15:30 +0200

    Create pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,6 @@
+import { GymSection } from "@/components/Pricing";
 import Section from "@/components/Section/Section";
 
 export default function Pricing() {
-  return (
-    <Section id="pricing">
-      <h2 className="text-4xl font-bold mb-4">Cennik</h2>
-    </Section>
-  );
+  return <GymSection />;
 }
