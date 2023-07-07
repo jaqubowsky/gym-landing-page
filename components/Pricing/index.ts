daaaf76dda6a6374452f20eb400e66613d24efa6
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-07 15:42:03 +0200

    Update index.ts

--- components/Pricing/index.ts
+++ components/Pricing/index.ts
@@ -1,2 +1,5 @@
-export {default as Pass } from './Pass';
+export { default as Pass } from "./Pass";
 export { default as GymSection } from "./GymSection";
+export { default as PricingSection } from "./PricingSection";
+export { default as FitnessSection } from "./FitnessSection";
+export { default as MartialArtsSection } from "./MartialArtsSection";
