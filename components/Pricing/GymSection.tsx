b83ec53e13179995d64e2317ba3529606ef0e880
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-07 15:42:23 +0200

    Use mapping for creating passes

--- components/Pricing/GymSection.tsx
+++ components/Pricing/GymSection.tsx
@@ -5,6 +5,25 @@ import Section from "../Section/Section";
 import ToggleOption from "../ToggleOption";
 import Pass from "./Pass";
 
+const passes = [
+  {
+    price: 120,
+    benefits: ["4 wejścia", "Ważny we wszystkich klubach", "Wstęp do siłowni"],
+  },
+  {
+    price: 135,
+    benefits: ["8 wejśc", "Ważny we wszystkich klubach", "Wstęp do siłowni"],
+  },
+  {
+    price: 145,
+    benefits: ["12 wejśc", "Ważny we wszystkich klubach", "Wstęp do siłowni"],
+  },
+  {
+    price: 155,
+    benefits: ["OPEN", "Ważny we wszystkich klubach", "Wstęp do siłowni"],
+  },
+];
+
 const GymSection = () => {
   const [isOptionOn, setIsOptionOn] = useState(false);
 
@@ -12,10 +31,19 @@ const GymSection = () => {
     setIsOptionOn(!isOptionOn);
   };
 
-  console.log(isOptionOn)
+  const passesEl = passes.map((pass) => {
+    return (
+      <Pass
+        isOptionOn={isOptionOn}
+        passPrice={pass.price}
+        benefits={pass.benefits}
+        key={pass.price}
+      />
+    );
+  });
 
   return (
-    <Section id="gym" className="bg-neutral-800" innerStyle="md:max-w-4xl">
+    <>
       <h2 className="text-6xl text-center font-bold mb-8 text-white drop-shadow-sm">
         Siłownia
       </h2>
@@ -25,8 +53,10 @@ const GymSection = () => {
         labelLeft="Wejście bez limitu"
         labelRight="Wejście do 16"
       />
-      <Pass isOptionOn={isOptionOn} passPrice={120}/>
-    </Section>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {passesEl}
+      </div>
+    </>
   );
 };
 
