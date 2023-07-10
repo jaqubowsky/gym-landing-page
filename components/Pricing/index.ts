94d240c4f9ef755ac83fb96783b8622dbe767659
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-10 14:54:47 +0200

    Delete PricingSection, add NotSelected

--- components/Pricing/index.ts
+++ components/Pricing/index.ts
@@ -1,5 +1,5 @@
 export { default as Pass } from "./Pass";
 export { default as GymSection } from "./GymSection";
-export { default as PricingSection } from "./PricingSection";
 export { default as FitnessSection } from "./FitnessSection";
 export { default as MartialArtsSection } from "./MartialArtsSection";
+export { default as NotSelected } from "./NotSelected";
