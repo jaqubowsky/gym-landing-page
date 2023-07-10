c15ef52a7aa4a255af40d3e9220610ac7821eac2
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-10 14:56:44 +0200

    Delete page.tsx

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import Section from "@/components/Section/Section";
-
-export default function Contact() {
-  return (
-    <Section id="contact">
-      <h2 className="text-4xl font-bold mb-4">Skontaktuj się z nami!</h2>
-    </Section>
-  );
-}
