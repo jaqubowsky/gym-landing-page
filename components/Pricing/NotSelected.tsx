7a38bd8d5efa90c69fee80035cb19a65712f11d0
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-10 14:53:34 +0200

    Create NotSelected.tsx

---
+++ components/Pricing/NotSelected.tsx
@@ -0,0 +1,11 @@
+const NotSelected = () => {
+  return (
+    <div className="mb-8">
+      <p className="text-gray-300 text-sm mt-2">
+        Zakup możliwy tylko stancjonarnie przy recepcji.
+      </p>
+    </div>
+  );
+};
+
+export default NotSelected;
