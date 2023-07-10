202a1126dda17da424fcb6ceea1c4cf44d7b4128
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-10 14:56:09 +0200

    Bigger h2

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -4,7 +4,7 @@ import Section from "@/components/Section/Section";
 export default function Scheudle() {
   return (
     <Section id="scheudle">
-      <h2 className="text-4xl font-bold mb-4">Grafik</h2>
+      <h2 className="text-6xl font-bold mb-4">Grafik</h2>
       <h5 className="font-bold mb-8">
         Jesteśmy dostępni dla ciebie cały tydzień!
       </h5>
