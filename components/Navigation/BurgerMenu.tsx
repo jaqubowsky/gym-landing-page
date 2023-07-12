daed0674ce9774ab3b9231da0da1d86be429ed0c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-12 12:11:05 +0200

    Fix bug

--- components/Navigation/BurgerMenu.tsx
+++ components/Navigation/BurgerMenu.tsx
@@ -13,8 +13,8 @@ const BurgerMenu: FC<BurgerMenu> = ({ children, isOpen, toggleOpen }) => {
     <>
       <Burger isOpen={isOpen} onClick={toggleOpen} />
       <div
-        className={`absolute transition-all filter flex duration-300 delay-75 left-0 top-0 w-screen h-screen bg-neutral-700 items-center justify-center opacity-0 ${
-          isOpen ? "slideIn" : ""
+        className={`absolute transition-all filter duration-300 delay-75 left-0 top-0 w-screen h-screen bg-neutral-700 items-center justify-center ${
+          isOpen ? "slideIn flex" : "hidden"
         } `}
       >
         <div className="flex flex-col items-center justify-around h-3/4">
