9cbc6b97d7824ca971424c708ad49be76d9f0817
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-12 11:25:06 +0200

    delete image

--- components/Navigation/BurgerMenu.tsx
+++ components/Navigation/BurgerMenu.tsx
@@ -15,16 +15,6 @@ const BurgerMenu: FC<BurgerMenu> = ({ children, isOpen, toggleOpen }) => {
       {isOpen && (
         <div className="absolute slideIn left-0 top-0 w-screen h-screen bg-neutral-700 flex items-center justify-center">
           <div className="flex flex-col items-center justify-around h-3/4">
-            <div className="h-48 w-48 flex-container">
-              <Image
-                src="https://res.cloudinary.com/doz2peb5r/image/upload/f_auto,q_auto/gym_logo_immgmy"
-                alt="logo"
-                className="object-contain w-full h-full"
-                height={200}
-                width={200}
-                priority
-              />
-            </div>
             {children}
           </div>
         </div>
